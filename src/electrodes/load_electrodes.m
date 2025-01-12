76b55865339497f3fdc59df89a5141658d69a67d
Tomáš Černík <54006748+cernito@users.noreply.github.com>
2025-01-13 00:37:24 +0100

    Delete src/electrodes directory

--- src/electrodes/load_electrodes.m
+++
@@ -1,8 +0,0 @@
-file_path = get_user_file_path('*.elc', 'elc');
-electrodes = elc_read(file_path);
-labels = electrodes.labels;
-Rz = [0 -1 0; 1 0 0; 0 0 1];
-positions = electrodes.pos * Rz;
-pcElectrodes = pointCloud(positions);
-
-pcwrite(pcElectrodes, 'electrodes.pcd', 'Encoding', 'ascii');
--- src/electrodes/plot_electrodes.m
+++
@@ -1,9 +0,0 @@
-
-figure(60); clf
-grayColor = [.7 .7 .7];
-pcshow(pcCap.Location, 'w')
-hold on
-pcshow(pcElectrodes.Location, 'r', 'MarkerSize', 200)
-hold on 
-pcshow(pcHead_scan)
-hold off
