fc0f79ae6d21a066c5456992520b789e11749cac
Tomáš Černík <54006748+cernito@users.noreply.github.com>
2025-01-13 00:38:45 +0100

    Delete src/file_loading directory

--- src/file_loading/load_files.m
+++
@@ -1,9 +0,0 @@
-function [mri_t1, T_matrix] = load_files(full_file_path)
-% Load spm data from the path
-
-header_info = spm_vol(full_file_path);
-data_size = header_info.dim;
-mri_t1 = spm_read_vols(header_info);
-T_matrix = header_info.mat;  % transformation matrix
-
-end
